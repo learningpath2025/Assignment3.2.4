86e608ec8607f42837e1d4efa8777a6e5d9e0830
learningpath2025 <learningpath2025@outlook.com>
2025-09-09 08:41:27 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Assignment3._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] NumberArray = GetNumbers(GetCount());
+            double total = CalculateTotal(NumberArray);
+            double average = CalculateAverage(total, NumberArray.Length);
+        }
+
+        static int GetCount()
+        {
+            Console.Write("Enter the count of numbers: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static double[] GetNumbers(int count)
+        {
+            double[] NumberArray = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                NumberArray[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            return NumberArray;
+        }
+
+        static double CalculateTotal(double[] NumberArray)
+        {
+            double sum = 0;
+            foreach (double number in NumberArray)
+            {
+                sum += number;
+            }
+            Console.WriteLine($"The total is: {sum}");
+            return sum;
+        }
+
+        static double CalculateAverage(double total, int count)
+        {
+            double average = total / count;
+            Console.WriteLine($"The average is: {average}");
+            return average;
+        }
+    }
+}
